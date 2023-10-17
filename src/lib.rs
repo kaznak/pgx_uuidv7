171f578fe199ddc468dee8c4884868c5734a17cc
kaznak <kaznak.at.work@ipebble.org>
2023-10-17 12:03:44 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ fn timestamptz_to_uuid_v7_random(ts: pgrx::TimestampWithTimeZone) -> pgrx::Uuid 
 }
 
 // #[pg_extern(immutable, parallel_safe)] // TODO make this public
-fn timestamptz_to_uuid_v7(ts: pgrx::TimestampWithTimeZone, rv: u32) -> pgrx::Uuid {
+fn timestamptz_to_uuid_v7(ts: pgrx::TimestampWithTimeZone, rv: u128) -> pgrx::Uuid {
     let u: uuid::Uuid = uuid::Builder::from_unix_timestamp_millis(
         to_uuid_timestamp_buildpart(ts),
         rv.to_be_bytes()[..10].try_into().unwrap(),
@@ -43,12 +43,12 @@ fn timestamptz_to_uuid_v7(ts: pgrx::TimestampWithTimeZone, rv: u32) -> pgrx::Uui
 
 #[pg_extern(immutable, parallel_safe)]
 fn timestamptz_to_uuid_v7_min(ts: pgrx::TimestampWithTimeZone) -> pgrx::Uuid {
-    timestamptz_to_uuid_v7(ts, std::u32::MIN.into())
+    timestamptz_to_uuid_v7(ts, std::u128::MIN)
 }
 
 #[pg_extern(immutable, parallel_safe)]
 fn timestamptz_to_uuid_v7_max(ts: pgrx::TimestampWithTimeZone) -> pgrx::Uuid {
-    timestamptz_to_uuid_v7(ts, std::u32::MAX.into())
+    timestamptz_to_uuid_v7(ts, std::u128::MAX)
 }
 
 extension_sql!(
@@ -71,12 +71,14 @@ mod tests {
         assert_eq!(7, u.get_version_num());
     }
 
+    fn gen_pt() -> pgrx::TimestampWithTimeZone {
+        pgrx::TimestampWithTimeZone::with_timezone(2012, 3, 4, 5, 6, 7.123456789, "UTC").unwrap()
+    }
+
     #[pg_test]
-    fn test_pgx_uuidv7() {
-        let pt000: pgrx::TimestampWithTimeZone =
-            pgrx::TimestampWithTimeZone::with_timezone(2012, 3, 4, 5, 6, 7.123456789, "UTC")
-                .unwrap();
-        let g: pgrx::Uuid = uuid_generate_v7(pt000); // <-- calling
+    fn test_pgx_uuidv7_new() {
+        let pt = gen_pt();
+        let g: pgrx::Uuid = uuid_generate_v7(pt); // <-- calling
         let u: uuid::Uuid = Converter(g).into();
         assert_eq!(7, u.get_version_num());
 
@@ -94,6 +96,35 @@ mod tests {
             pgrx::TimestampWithTimeZone::with_timezone(2012, 3, 4, 5, 6, 7.123, "UTC").unwrap();
         assert_eq!(pt001, pt002);
     }
+
+    #[pg_test]
+    fn test_pgx_uuidv7_min() {
+        let pt = gen_pt();
+        let u_min: pgrx::Uuid = timestamptz_to_uuid_v7_min(pt); // <-- calling
+        let u: uuid::Uuid = Converter(u_min).into();
+        assert_eq!(7, u.get_version_num());
+    }
+
+    #[pg_test]
+    fn test_pgx_uuidv7_max() {
+        let pt = gen_pt();
+        let u_min: pgrx::Uuid = timestamptz_to_uuid_v7_max(pt); // <-- calling
+        let u: uuid::Uuid = Converter(u_min).into();
+        assert_eq!(7, u.get_version_num());
+    }
+
+    #[pg_test]
+    fn test_pgx_uuidv7_order() {
+        let pt = gen_pt();
+        let u_min: pgrx::Uuid = timestamptz_to_uuid_v7_min(pt); // <-- calling
+        let u_rnd: pgrx::Uuid = timestamptz_to_uuid_v7_random(pt); // <-- calling
+        let u_max: pgrx::Uuid = timestamptz_to_uuid_v7_max(pt); // <-- calling
+        assert!(u_min < u_max);
+        assert!(u_min <= u_rnd);
+        assert!(u_rnd <= u_max);
+    }
+    /*
+     */
 }
 
 /// This module is required by `cargo pgrx test` invocations.
